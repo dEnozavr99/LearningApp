c211c64e270e8a5eb4477a0d5526f1620f05a23f
Denys Rudyk <rudykdenys@gmail.com>
2023-05-31 22:17:00 +0300

    Update base screen wrapper

--- src/component/BaseScreenView.tsx
+++ src/component/BaseScreenView.tsx
@@ -2,7 +2,13 @@ import { View, Text } from "react-native";
 import React from "react";
 import Colors from "../theme/colors";
 
-const BaseScreenView = ({ children, style }: { children: any; style: any }) => {
+const BaseScreenView = ({
+  children,
+  style,
+}: {
+  children: any;
+  style?: any;
+}) => {
   return (
     <View style={[{ backgroundColor: Colors.background }, style]}>
       {children}
