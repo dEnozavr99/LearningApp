8d1527d572cf2e68206d8b446d27befbade98992
Denys Rudyk <rudykdenys@gmail.com>
2023-05-31 20:14:29 +0300

    Add colors

---
+++ src/theme/colors.ts
@@ -0,0 +1,10 @@
+const Colors = {
+  background: "#335C67",
+  text: "#FFF3B0",
+  title: "#E09F3E",
+  primary: "#9E2A2B",
+  secondary: "#540B0E",
+  white: "#FFFFFF",
+};
+
+export default Colors;
